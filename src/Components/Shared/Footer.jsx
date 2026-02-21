409b7f3f65cf52d3060e7c0a4ecf8e6e558b7fb4
sh-shahedul <shahedulhoque2003@gmail.com>
2026-02-21 15:24:33 +0600

    responsive footer

--- src/Components/Shared/Footer.jsx
+++ src/Components/Shared/Footer.jsx
@@ -84,7 +84,7 @@ import group from "../../../src/assets/Group1.png"
 
 const Footer = () => {
   return (
-    <footer className=" mt-12 px-12 max-w-7xl mx-auto ">
+    <footer className=" mt-12 md:px-12 px-5 max-w-7xl mx-auto ">
       {/* Top Blue Section */}
       <div className="relative  bg-blue-600 rounded-4xl">
         <div className=" text-white px-5 md:px-12 py-10 flex flex-col md:flex-row justify-between items-center rounded-t-3xl ">
@@ -102,9 +102,9 @@ const Footer = () => {
             </button>
           </div>
         </div>
-        <div>
+        <div className=" w-full flex md:justify-end">
             <img src={group} alt="" 
-            className="w-[350px] h-[85px] mr-[80px]"
+            className="md:w-87 w-47 h-12 md:h-21 md:mr-20 "
             />
         </div>
       </div>
@@ -117,10 +117,10 @@ const Footer = () => {
 
         <div className="grid grid-cols-1 md:grid-cols-5 gap-10 relative z-20">
           {/* About Us */}
-          <div className="col-span-2">
+          <div className="md:col-span-2">
             <h3 className="font-bold text-lg mb-2 text-orange-400">About us</h3>
-            <p className="text-base text-gray-300">
-              We are the biggest hyperstore in the universe. <br /> We got you all cover with our exclusive <br /> collections and latest drops.
+            <p className="text-base text-gray-300 md:pr-15 pr-3" >
+              We are the biggest hyperstore in the universe.  We got you all cover with our exclusive collections and latest drops.
             </p>
           </div>
 
